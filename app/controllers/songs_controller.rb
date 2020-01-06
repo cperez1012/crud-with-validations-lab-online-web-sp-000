2076bb7fc137b4f634748219912c49670e339f6b
Christian Perez <perezchristian1012@gmail.com>
2020-01-06 22:02:17 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
     before_action :set_song, only: [:show, :edit, :update, :destroy]
     def index
-        @songs = Song.all
+      @songs = Song.all
     end
 
     def show
@@ -18,7 +18,8 @@ class SongsController < ApplicationController
 
     def create
       @song = Song.new(song_params)
-      if @song.save
+      if @song.valid?
+        @song.save
         redirect_to song_path(@song)
       else
         render :new
