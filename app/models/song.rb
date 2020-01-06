1676414bd9fa08589ee05c8da6ef2f310e3414eb
Christian Perez <perezchristian1012@gmail.com>
2020-01-06 22:06:13 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,6 @@
-class Song < ActiveRecord::Base
-  validates :title, presence: true
-  validates :release_year, inclusion: {in: [true, false]}
-  with_options if: :released do |s|
-      s.validates :release_year, presence: true
-      s.validates :release_year, numericality: {less_than_or_equal_to: Date.today.year}
-  end
-
-  validates :title, uniqueness: {scope: %i[release_year artist_name]}
+class Song < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates_presence_of :release_year, :if => :released?
+  validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }, :if => :released?
+  validates :artist_name, presence: true
 end
